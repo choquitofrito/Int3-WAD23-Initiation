2dd7a0897cc05218c4e312265a16d4135a939003
choquitofrito <zyriab@gmail.com>
2023-04-12 15:53:50 +0200

    if 02

--- CodeCours/PHPConsole/Exercices/exIf01.php
+++ CodeCours/PHPConsole/Exercices/exIf01.php
@@ -1,1 +1,11 @@
 <?php
+
+$longueur = readLine ("Tapez la longueur de la chambre (m): ");
+$largeur = readLine ("Tapez la largeur de la chambre (m): ");
+$surface = $longueur * $largeur;
+
+print ("\nLa surface de la chambre est de : " . $surface . " m2");
+
+if ($longueur == $largeur) {
+    print ("\nLa chambre est carrée");
+}
