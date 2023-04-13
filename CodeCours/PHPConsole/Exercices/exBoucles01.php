58dab88f177050161f7398b82e919ab5102a6661
choquitofrito <zyriab@gmail.com>
2023-04-13 11:28:57 +0200

    ex01 corrigé

--- CodeCours/PHPConsole/Exercices/exBoucles01.php
+++ CodeCours/PHPConsole/Exercices/exBoucles01.php
@@ -1,6 +1,6 @@
 <?php
 $age = (int)readline ("vous avez quel age? ");
-while ($age < 0 AND $age > 130 ) {
+while ($age < 0 || $age > 130 ) {
     
     $age = readline ("vous avez quel age? ");
       
