c8a5896a414554f90f1b6c9dab31c2e1ed09af37
choquitofrito <zyriab@gmail.com>
2023-04-13 10:16:03 +0200

    exemple boucles

--- CodeCours/PHPConsole/Exemples/exemplesBoucles1.php
+++ CodeCours/PHPConsole/Exemples/exemplesBoucles1.php
@@ -9,11 +9,31 @@ while ($i <= 30) {  // 2. Condition
     $i++;  // 3. Changement des variables de la condition (incrémentation d'un compteur)
 }
 
+// $i = $i + 1; ------> $i++;
+// $i = $i + 5; ------> $i+=5;
+
 // 1. Initialisation ($i = 20)
 // 2. Condition ($i <= 30)
 // 3. Changement des variables de la condition (incrémentation d'un compteur) ($i++)
-for ($i = 20; $i <= 30; $i++){
+for ($i = 20; $i <= 30; $i+=2){
     print($i . "\n");
 }
 
+
+// $somme = 0;
+
+// $somme = $somme + 1;
+// $somme = $somme + 2;
+// $somme = $somme + 3;
+// $somme = $somme + 4;
+// .
+// .
+// print ($somme)
+
+// afficher la somme des valeurs entre 1 et 5;
+$somme = 0;
+for ($i = 1; $i < 5; $i++) {
+    $somme = $somme + $i;
+}
+print ($somme);
 
