03340d8e42c5a4f0aa44cae1f1a99cd4c714e74f
choquitofrito <zyriab@gmail.com>
2023-04-13 09:49:44 +0200

    boucle for

---
+++ CodeCours/PHPConsole/Exemples/exemplesBoucles1.php
@@ -0,0 +1,19 @@
+<?php
+
+// boucle pour afficher les valeurs de 20 à 30
+// (while)
+
+$i = 20;  // 1. Initialisation
+while ($i <= 30) {  // 2. Condition  
+    print($i . "\n");
+    $i++;  // 3. Changement des variables de la condition (incrémentation d'un compteur)
+}
+
+// 1. Initialisation ($i = 20)
+// 2. Condition ($i <= 30)
+// 3. Changement des variables de la condition (incrémentation d'un compteur) ($i++)
+for ($i = 20; $i <= 30; $i++){
+    print($i . "\n");
+}
+
+
