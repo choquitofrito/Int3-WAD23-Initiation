bc85b0393c6ba7c9e4e854fd016c78ee2154126e
choquitofrito <zyriab@gmail.com>
2023-04-14 10:21:24 +0200

    exo 6 arrays

--- CodeCours/PHPConsole/Exercices/exArrays06.php
+++ CodeCours/PHPConsole/Exercices/exArrays06.php
@@ -1,2 +1,29 @@
 <?php
 
+$chiffres = [];
+
+// $chiffres[] = 1;
+// $chiffres[] = 2;
+// $chiffres[] = 3;
+// $chiffres[] = 4;
+// .
+// .
+// $tab = range(1,200);
+
+// foreach (range(1,200) as $chiffre){
+//     print ("\n".$chiffre);
+// }
+// die();
+
+for ($i = 1; $i <= 200; $i++){
+    $chiffres[] = $i;
+}
+
+// for ($i = 0; $i < 200; $i++){
+//     $chiffres[$i] = $i + 1;
+// }
+
+foreach ($chiffres as $chiffre){
+    print ("\n". $chiffre);
+}
+// print_r ($chiffres);
