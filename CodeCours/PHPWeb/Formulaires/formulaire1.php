7c95bab7242218742b657e15d44d1bb887b6da8e
choquitofrito <zyriab@gmail.com>
2023-04-25 13:53:03 +0200

    formbase

---
+++ CodeCours/PHPWeb/Formulaires/formulaire1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="./formulaire1Traitement.php">
+    Nom<input type="text" name="nom">
+    Prénom<input type="text" name="prenom">
+    Date de naissance<input type="date" name="dateNaissance">
+
+    Hobby
+    <select name="hobby">
+        <option value="manger">Manger</option>
+        <option value="natation">Natation</option>
+        <option value="cinema">Cinema</option>
+        <option value="tricot">Tricot</option>
+    </select>
+
+    <input type="submit" value="Envoyer">
+</form>
+
+
+
+
+</body>
+</html>
